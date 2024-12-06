c7f9a74627577f8f156db62b07280527c35b862a
Miro193 <mirostonezone@gmail.com>
2024-12-06 20:30:39 +0200

    tehtävät 2 ja 3  palautus

--- mod4/teht2/m4t2.js
+++ mod4/teht2/m4t2.js
@@ -1,13 +1,14 @@
 'use strict';
 
-const apiForm = document.querySelector('form');
-apiForm.addEventListener('submit', async function (evt) {
+document.addEventListener('submit', async function (evt) {
+  // ... prevent the default action.
   evt.preventDefault();
-  const code = document.querySelector('#query').value;
+  // get value of input element
+  const code = document.querySelector('input[name=q]').value;
   try {                                               // error handling: try/catch/finally
     const response = await fetch(`https://api.tvmaze.com/search/shows?q=${code}`);    // starting data download, fetch returns a promise which contains an object of type 'response'
     const jsonData = await response.json();          // retrieving the data retrieved from the response object using the json() function
-    console.log(jsonData.ICAO, jsonData.Name);    // log the result to the console
+    console.log(jsonData);    // log the result to the console
   } catch (error) {
     console.log(error.message);
   }
